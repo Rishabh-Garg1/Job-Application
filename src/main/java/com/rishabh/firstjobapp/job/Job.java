7dfba94e6a9791df4fe2323bd4c4d9f21a2446dd
Rishabh Garg <66793718+Rishabh-Garg1@users.noreply.github.com>
2025-12-13 23:03:35 +0530

    applied jpa and h2 database
    to access h2 console http://localhost:8080/h2-console
    and jdbc:h2:mem:test is jdbc url as specified in application.yml

--- src/main/java/com/rishabh/firstjobapp/job/Job.java
+++ src/main/java/com/rishabh/firstjobapp/job/Job.java
@@ -1,6 +1,14 @@
 package com.rishabh.firstjobapp.job;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
